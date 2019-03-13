77e48f85e04aac05cbbe8721dbb7b54fc78bd339
Eric Tepper <eric.tepper@me.com>
2019-03-13 00:04:10 -0700

    Renamed variables and classes for clarity.

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -4,18 +4,18 @@ import java.awt.event.*;
 
 
 public class Minesweeper implements MouseListener {
-    private GamePanel GAME_PANEL;
+    private MinesweeperDisplay DISPLAY;
 
     private Minesweeper(String size) {
         GameGrid gameGrid = new GameGrid(size, 50, 200, 30);
 
         JFrame gameFrame = new JFrame("Minesweeper");
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GAME_PANEL = new GamePanel(50, 200, 30, gameGrid);
-        GAME_PANEL.setSize(1000, 730);
-        GAME_PANEL.setBackground(Color.WHITE);
-        GAME_PANEL.addMouseListener(this);
-        gameFrame.add(GAME_PANEL);
+        DISPLAY = new MinesweeperDisplay(50, 200, 30, gameGrid);
+        DISPLAY.setSize(1000, 730);
+        DISPLAY.setBackground(Color.WHITE);
+        DISPLAY.addMouseListener(this);
+        gameFrame.add(DISPLAY);
         gameFrame.setSize(1000, 730);
         gameFrame.setVisible(true);
     }
@@ -27,9 +27,9 @@ public class Minesweeper implements MouseListener {
     public void mouseClicked(MouseEvent e) { }
 
     public void mousePressed(MouseEvent e) {
-        GAME_PANEL.mouseClicked(e.getX(), e.getY(), e.getButton());
-        if (GAME_PANEL.getGameStatus()) {
-            GAME_PANEL.removeMouseListener(this);
+        DISPLAY.mouseClicked(e.getX(), e.getY(), e.getButton());
+        if (DISPLAY.getGameStatus()) {
+            DISPLAY.removeMouseListener(this);
         }
     }
 
--- src/MinesweeperDisplay.java
+++ src/MinesweeperDisplay.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-class GamePanel extends JPanel {
+class MinesweeperDisplay extends JPanel {
     private int BOARD_START_X;
     private int BOARD_START_Y;
     private int SQUARE_SIZE;
@@ -12,7 +12,7 @@ class GamePanel extends JPanel {
     private int NUMBER_OF_FLAGS;
     private int NUMBER_OF_REVEALED_NUMBERS;
 
-    GamePanel(int boardXStart, int boardYStart, int boardSquareSize, GameGrid gameGrid) {
+    MinesweeperDisplay(int boardXStart, int boardYStart, int boardSquareSize, GameGrid gameGrid) {
         BOARD_START_X = boardXStart;
         BOARD_START_Y = boardYStart;
         SQUARE_SIZE = boardSquareSize;
