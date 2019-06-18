134df752824dbd3a6542fa9b05e1b8fa2ceb8d5d
erictepper <eric.tepper@me.com>
2019-06-18 02:21:34 -0700

    Fixed formatting to conform to Google's style guide https://google.github.io/styleguide/javaguide.html

--- src/GameTile.java
+++ src/GameTile.java
@@ -1,76 +1,76 @@
 class GameTile {
-    private int displayStatus;  // 0 for hidden tile, 1 for number tile, 2 for mine tile, 3 for flag tile, 4 for
-                                 // false-flag tile
-    private int tileType;  // 1 for number tile, 2 for mine tile
-    private int adjacentMinesCount;
+  private int displayStatus;  // 0 for hidden tile, 1 for number tile, 2 for mine tile, 3 for flag tile, 4 for
+                              // false-flag tile
+  private int tileType;  // 1 for number tile, 2 for mine tile
+  private int adjacentMinesCount;
 
-    GameTile() {
-        displayStatus = 0;
-        tileType = 1;
-        adjacentMinesCount = 0;
-    }
+  GameTile() {
+    displayStatus = 0;
+    tileType = 1;
+    adjacentMinesCount = 0;
+  }
 
-    void setTileType(int tileType) {
-        this.tileType = tileType;
-    }
+  void setTileType(int type) {
+    tileType = type;
+  }
 
-    void setAdjacentMinesCount(int count) {
-        adjacentMinesCount = count;
-    }
+  void setAdjacentMinesCount(int count) {
+    adjacentMinesCount = count;
+  }
 
-    // Returns 0 if the display status didn't change, 1 if it revealed a number square, and 2 if it revealed a
-    // mine square.
-    int reveal() {
-        if (displayStatus == tileType || displayStatus == 3) { return 0; }
-        else {
-            displayStatus = tileType;
-            return tileType;
-        }
+  // Returns 0 if the display status didn't change, 1 if it revealed a number square, and 2 if it revealed a
+  // mine square.
+  int reveal() {
+    if (displayStatus == tileType || displayStatus == 3) { return 0; }
+    else {
+      displayStatus = tileType;
+      return tileType;
     }
+  }
 
-    // Marks the tile as a false flag if it is a number tile and has been flagged, otherwise does nothing.
-    void setFalseFlag() {
-        if (displayStatus == 3 && tileType == 1) {
-            displayStatus = 4;
-        }
+  // Marks the tile as a false flag if it is a number tile and has been flagged, otherwise does nothing.
+  void setFalseFlag() {
+    if (displayStatus == 3 && tileType == 1) {
+      displayStatus = 4;
     }
+  }
 
-    // Changes the flagged status of a hidden tile.
-    // Returns 1 if this tile is originally un-flagged and becomes flagged,
-    // returns -1 if it is originally flagged and becomes un-flagged,
-    // else returns 0 if the position is already revealed (i.e. can't be flagged).
-    int flag() {
-        if (displayStatus == 0) { displayStatus = 3; return 1; }
-        else if (displayStatus == 3) { displayStatus = 0; return -1; }
-        else return 0;
-    }
+  // Changes the flagged status of a hidden tile.
+  // Returns 1 if this tile is originally un-flagged and becomes flagged,
+  // returns -1 if it is originally flagged and becomes un-flagged,
+  // else returns 0 if the position is already revealed (i.e. can't be flagged).
+  int flag() {
+    if (displayStatus == 0) { displayStatus = 3; return 1; }
+    else if (displayStatus == 3) { displayStatus = 0; return -1; }
+    else return 0;
+  }
 
-    void reset() {
-        displayStatus = 0;
-    }
+  void reset() {
+    displayStatus = 0;
+  }
 
-    String label() {
-        if (displayStatus == 0) { return ""; }
-        else if (displayStatus == 1) {
-            if (adjacentMinesCount == 0) { return ""; }
-            else return String.valueOf(adjacentMinesCount);
-        }
-        else if (displayStatus == 2) { return "M"; }
-        else if (displayStatus == 3) { return "F"; }
-        else {
-            throw new IndexOutOfBoundsException();
-        }
+  String label() {
+    if (displayStatus == 0) { return ""; }
+    else if (displayStatus == 1) {
+      if (adjacentMinesCount == 0) { return ""; }
+      else return String.valueOf(adjacentMinesCount);
     }
+    else if (displayStatus == 2) { return "M"; }
+    else if (displayStatus == 3) { return "F"; }
+    else {
+      throw new IndexOutOfBoundsException();
+    }
+  }
 
-    int getDisplayStatus() {
-        return displayStatus;
-    }
+  int getDisplayStatus() {
+    return displayStatus;
+  }
 
-    int getActualStatus() {
-        return tileType;
-    }
+  int getActualStatus() {
+    return tileType;
+  }
 
-    int getAdjacentMinesCount() {
-        return adjacentMinesCount;
-    }
+  int getAdjacentMinesCount() {
+    return adjacentMinesCount;
+  }
 }
